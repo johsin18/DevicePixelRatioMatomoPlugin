0def3f2fc900123cbfbe5439d265f2c9ff0b3ebd
Johannes Singler <johannes@singler.name>
2018-07-14 17:02:59 +0200

    Generated Archiver.

---
+++ Archiver.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\DevicePixelRatio;
+
+/**
+ * Class Archiver
+ * @package Piwik\Plugins\DevicePixelRatio
+ *
+ * Archiver is class processing raw data into ready ro read reports.
+ * It must implement two methods for aggregating daily reports
+ * aggregateDayReport() and other for summing daily reports into periods
+ * like week, month, year or custom range aggregateMultipleReports().
+ *
+ * For more detailed information about Archiver please visit Piwik developer guide
+ * http://developer.piwik.org/api-reference/Piwik/Plugin/Archiver
+ */
+class Archiver extends \Piwik\Plugin\Archiver
+{
+    const DEVICEPIXELRATIO_ARCHIVE_RECORD = "DevicePixelRatio_archive_record";
+
+    public function aggregateDayReport()
+    {
+         *
+         * $visitorMetrics = $this
+         * ->getLogAggregator()
+         * ->getMetricsFromVisitByDimension('idvisitor')
+         * ->asDataTable();
+         * $visitorReport = $visitorMetrics->getSerialized();
+         * $this->getProcessor()->insertBlobRecord(self::DEVICEPIXELRATIO_ARCHIVE_RECORD, $visitorReport);
+    }
+
+    public function aggregateMultipleReports()
+    {
+         *
+         * $this->getProcessor()->aggregateDataTableRecords(self::DEVICEPIXELRATIO_ARCHIVE_RECORD);
+    }
+}
